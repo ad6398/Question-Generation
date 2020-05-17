64c500862a639b22b7b9739156e77ddca600cab4
AMARDEEP KUMAR <38162294+ad6398@users.noreply.github.com>
2020-05-17 16:10:14 +0530

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -7,28 +7,28 @@ max_seq_len =256
 class trainDataArgs:
     bert_model= bert_model
     max_seq_len = max_seq_len
-    squad_path= '../input/stanford-question-answering-dataset/train-v1.1.json' #CR
+    squad_path= 'train-v1.1.json' #CR
     inferenceMode= False  # T/F for inference
     inferenceFile= None  # if nferenceMode = True: a path to json file consisting of list of dict with keys 'paragraph' as mandatory key and 'context_list' as option  [{'paragraph':' a string', 'context_list': ['list' , 'of', 'context', 'i.e answers'] }, {}, . . .]
 
-    occu = 30000
+    occu = None
 
 class validDataArgs:
     bert_model= bert_model
     max_seq_len =max_seq_len
-    squad_path= '../input/stanford-question-answering-dataset/dev-v1.1.json' #CR
+    squad_path= 'dev-v1.1.json' #CR
     inferenceMode= False  # T/F for inference
     inferenceFile= None  # if nferenceMode = True: a path to json file consisting of list of dict with keys 'paragraph' as mandatory key and 'context_list' as option  [{'paragraph':' a string', 'context_list': ['list' , 'of', 'context', 'i.e answers'] }, {}, . . .]
-    occu= 8000
+    occu= 10000
 
 class inferenceArgs:
     bert_model= bert_model
     max_seq_len =max_seq_len
-    squad_path= '/kaggle/input/stanford-question-answering-dataset/dev-v1.1.json' #CR
+    squad_path= 'dev-v1.1.json' #CR
     inferenceMode= False  # T/F for inference
     inferenceFile= None  # CR
     # if nferenceMode = True: a path to json file consisting of list of dict with keys 'paragraph' as mandatory key and 'context_list' as option  [{'paragraph':' a string', 'context_list': ['list' , 'of', 'context', 'i.e answers'] }, {}, . . .]
-    occu= 32
+    occu= None
     infereceModelPath= '/kaggle/input/qg-bert-en-dec/model_2'
 
 
@@ -40,6 +40,6 @@ class trainingConfig:
     valid_batch_size= 16
     ignore_label =0
     num_workers=0
-    epochs =3
+    epochs =10
     learningRate = 5e-5
-    save_dir = '/kaggle/working/'
+    save_dir = ''
